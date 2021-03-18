8122a3a40a6c066ad0e265175dca491f748b0c64
Jesus Coello <jescoevas@hotmail.com>
2021-03-18 19:19:42 +0100

    Login

---
+++ src/routes/usuario.routes.js
@@ -0,0 +1,25 @@
+const { Router } = require('express')
+const Usuario = require('../models/usuario')
+const Token = require('../tools/token')
+
+const router = Router()
+
+router.post('/login', async(req, res) => {
+    const { email, password } = req.body
+    const usuario = await Usuario.findOne({ email })
+    if (!usuario)
+        return res.json({ msg: "No se ha encontrado el usuario" })
+            //const coincide = await usuario.compararPassword(password)
+    const coincide = password == usuario.password
+    if (!coincide)
+        return res.json({ msg: "Password incorrecta" })
+    res.json({
+        msg: "Login realizado con exito",
+        usuarioId: usuario._id,
+        token: Token.getJwtToken(usuario)
+    })
+})
+
+
+
+module.exports = router

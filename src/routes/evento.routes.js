1d558bab3cb82112105d6fb0d44ef85b3136a0d6
Jesus Coello <jescoevas@hotmail.com>
2021-03-21 19:18:26 +0100

    Correcciones

--- src/routes/evento.routes.js
+++ src/routes/evento.routes.js
@@ -1,22 +1,22 @@
-const { Router, json } = require('express')
+const { Router } = require('express')
 const Evento = require('../models/evento')
 const verificarToken = require('../tools/verificarToken')
 
 
 const router = Router()
 
-router.get('/evento', async(res) => {
-    const eventos = await Evento.find() 
-    res.json(eventos)    
+router.get('/evento', async(req, resp) => {
+    const eventos = await Evento.find()
+    return resp.json({ eventos })
 })
 router.post('/evento', async(req, res) => {
-    const {_id} = req.body
-    const evento = await Evento.findById({_id})
+    const { _id } = req.body
+    const evento = await Evento.findById({ _id })
     res.json(evento)
 })
-router.post('/evento/mios', verificarToken, async(req, res) =>{
+router.post('/evento/mios', verificarToken, async(req, res) => {
     const usuario = req.usuario
-    const evento = await Evento.find({profesional:usuario._id})
+    const evento = await Evento.find({ profesional: usuario._id })
     res.json(evento)
 })
 

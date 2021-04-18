bee21aeb5413fc9b353df13937384b0b9f4a7ba1
serperrui1 <serperrui1@alum.us.es>
2021-04-18 16:32:11 +0200

    arreglos del 2 sprint

--- src/routes/zoom.routes.js
+++ src/routes/zoom.routes.js
@@ -7,9 +7,9 @@ const Usuario = require("../models/usuario");
 const ZoomDatosUsuarios = require("../models/zoomDatosUsuarios");
 const ZoomDatosReunion = require("../models/zoomDatosReunion");
 
-const ZOOM_CLIENT_ID = "BqYXOyymQ_OGhBXQKV653A";
-const ZOOM_CLIENT_SECRET = "5j4pldvoGkaK1VWW3fn9oojvC7hzq9Op";
-const ZOOM_REDIRECT_URI = "https://comfystream-frontend-s1.web.app/landing";
+const ZOOM_CLIENT_ID = "XkwgnLlHRmS3IhiX21ic9Q";
+const ZOOM_CLIENT_SECRET = "5X5qb1AFYdUHM6kEzw0tF6vYxujzXIdi";
+const ZOOM_REDIRECT_URI = "https://comfystream-s2.web.app/landing";
 
 const router = Router()
 

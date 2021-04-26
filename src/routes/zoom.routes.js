d572ae96f2e9aa81d474d6fbbd755dcd1b767aac
serperrui1 <serperrui1@alum.us.es>
2021-04-26 20:05:55 +0200

    credenciales zoom

--- src/routes/zoom.routes.js
+++ src/routes/zoom.routes.js
@@ -7,8 +7,8 @@ const Usuario = require("../models/usuario");
 const ZoomDatosUsuarios = require("../models/zoomDatosUsuarios");
 const ZoomDatosReunion = require("../models/zoomDatosReunion");
 
-const ZOOM_CLIENT_ID = "VT5sHWdTTMalMXqMqxQ5g";
-const ZOOM_CLIENT_SECRET = "A2UjhG8FSAvmgYRhljDlhvqpaiOq7H1f";
+const ZOOM_CLIENT_ID = "09dqbegGSzGQmtlHidI8fA";
+const ZOOM_CLIENT_SECRET = "plqSmo74lBf3KRfPYtdYJ6MtXOkai81y";
 const ZOOM_REDIRECT_URI = "https://comfystream-s3.web.app/landing";
 
 const router = Router()
